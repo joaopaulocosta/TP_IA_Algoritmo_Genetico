b5725a8e1becbb2a6c0ea925dd03d8ee7194771b
Joao <joaopaulomc14@hotmail.com>
2017-02-11 16:02:03 -0200

    Pequena alteracao em variavel

--- src/util/ag/Main.java
+++ src/util/ag/Main.java
@@ -3,9 +3,10 @@ package util.ag;
 public class Main {
 	
 	public static final int NUM_GERACOES = 50;
+	public static final int NUM_INDIVIDUOS = 20;
 	
 	public static void main(String [] args){
-		Populacao pop = new Populacao(10);
+		Populacao pop = new Populacao(NUM_INDIVIDUOS);
 		int contGeracoes = 1;
 		System.out.println("Geraçao: " + contGeracoes++);
 		pop.imprimirPopulacao();
